0133a6ea4cde8e7eddee9f7b2179bb0f7e3379e1
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-04-26 16:35:27 -0400

    Task: Fix indentation errors eslint in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -10,23 +10,23 @@ getJSON('', function (data) {
 });
 
 getJSON('http://localhost:8000/api/v1/cities',
-  function(err, records) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  }
-  else {
-    let table = document.querySelector('table');
-    let data = Object.keys((records.data[0]));
-    let dataRecords = records.data;
+  function (err, records) {
+    if (err !== null) {
+      alert('Something went wrong: ' + err);
+    }
+    else {
+      let table = document.querySelector('table');
+      let data = Object.keys((records.data[0]));
+      let dataRecords = records.data;
 
-    generateTableHead(table, data);
-    generateTable(table, dataRecords);
-    /*
+      generateTableHead(table, data);
+      generateTable(table, dataRecords);
+      /*
        let data = Object.keys(records.data[0]);
        generateTable(table, records.data); // generate the table first
        generateTableHead(table, data); // then the head
-    */
-  }
+      */
+    }
 
-});
+  });
 ConsoleLogIt('this worked in the bundle');
