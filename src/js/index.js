6cb6d14462ebc2d8c703e5847ffddc22dea460b6
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-04-26 16:46:54 -0400

    Task: Fix unexpected string concatenation error eslint in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,7 @@ getJSON('', function (data) {
 getJSON('http://localhost:8000/api/v1/cities',
   function (err, records) {
     if (err !== null) {
-      alert('Something went wrong: ' + err);
+      alert(`Something went wrong: , ${err}`);
     } else {
       const table = document.querySelector('table');
       const data = Object.keys((records.data[0]));
