ab374f3115782f8d956012810b3309be5aeca07d
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-04-26 16:21:40 -0400

    Task: Fix quotes error eslint in generateTableHead.js

--- src/js/generateTableHead.js
+++ src/js/generateTableHead.js
@@ -3,7 +3,7 @@ export default function generateTableHead(table, data) {
   const thead = table.createTHead();
   const row = thead.insertRow();
   for (const key of data) {
-    const th = document.createElement("th");
+    const th = document.createElement('th');
     const text = document.createTextNode(key);
     th.appendChild(text);
     row.appendChild(th);
