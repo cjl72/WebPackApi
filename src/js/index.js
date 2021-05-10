c4d1e9609a25ba9d51bedd7297e0f605cebdd172
cjl72 <cjl72@njit.edu>
2021-05-10 13:53:59 -0400

    Task: Created final repository off of express branch

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,30 @@
-import TestJS from "./TestJs.js";
-import ConsoleLogIt from "./ConsoleLogIt.js";
-import getJSON from "./getJSON";
-import generateTableHead from "./generateTableHead";
-import generateTable from "./generateTable";
+import TestJS from './TestJs.js';
+import ConsoleLogIt from './ConsoleLogIt.js';
+import getJSON from './getJSON';
+import generateTableHead from './generateTableHead';
+import generateTable from './generateTable';
 
 TestJS();
-getJSON("", function (data) {
-    console.log(data);
+getJSON('', (data) => {
+  console.log(data);
 });
 
 getJSON('http://localhost:8000/api/v1/cities',
-    function(err, records) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        } else {
-            let table = document.querySelector("table");
-            let data = Object.keys((records.data[0]));
-            let dataRecords = records.data;
+  (err, records) => {
+    if (err !== null) {
+      alert(`Something went wrong: ${err}`);
+    } else {
+      const table = document.querySelector('table');
+      const data = Object.keys((records.data[0]));
+      const dataRecords = records.data;
 
-            generateTableHead(table, data);
-            generateTable(table, dataRecords);
-            /*
+      generateTableHead(table, data);
+      generateTable(table, dataRecords);
+      /*
             let data = Object.keys(records.data[0]);
             generateTable(table, records.data); // generate the table first
             generateTableHead(table, data); // then the head
              */
-        }
-
-    });
-ConsoleLogIt("this worked in the bundle");
+    }
+  });
+ConsoleLogIt('this worked in the bundle');
